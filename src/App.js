6933f5d5dfdce82eab16d3644817464640cc507e
Imseesea <136055408+Imseesea@users.noreply.github.com>
2023-10-31 14:58:21 +0900

    react-comonents

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+//컴포넌트 생성
+
+import { Reset } from "styled-reset";
+import Article from "./components/Article";
+import Header from "./components/Header";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Reset />
+      <div className="root">
+        <Header />
+        <MainStyle className="main">
+          <Article />
+          <Article />
+          <Article />
+        </MainStyle>
+      </div>
+    </>
   );
 }
-
+const MainStyle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: calc(100% - 4rem);
+  margin: 20px auto 0;
+  article {
+    width: 30%;
+  }
+`;
 export default App;
